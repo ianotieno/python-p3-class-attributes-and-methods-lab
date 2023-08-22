8a1d6b1cb3a70a09f6d3689687c945d51afda461
ianotieno <ianotieno23@gmail.com>
2023-08-22 13:30:08 +0300

    third test

--- lib/song.py
+++ lib/song.py
@@ -1,12 +1,14 @@
 class Song:
     genres=[]
+    artists=[]
     count = 0
 
     def __init__(self,name):
         self.name=name
         self.add_song_to_count()
         Song.add_song_to_all(self)
-        
+        Song.add_to_artists(self)
+
     @classmethod
     def add_song_to_count(cls, increment=1):
         cls.count += increment
@@ -14,3 +16,7 @@ class Song:
     @classmethod
     def add_song_to_all(cls, song):
         cls.genres.append(song)
+        
+    @classmethod
+    def add_to_artists(cls, artist):
+        cls.artists.append(artist)
